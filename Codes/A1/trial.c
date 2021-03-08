b645b1787e4c7435804e55f850aaa353494c4e42
Xu Jing <13309618@student.uts.edu.au>
2021-03-09 01:21:25 +1100

    initial intSet type

--- Codes/A1/trial.c
+++ Codes/A1/trial.c
@@ -1,132 +1,67 @@
 //
 // Created by jing on 6/03/2021.
 //
-#include <stdio.h> //预编译符
+#include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
-/**
- * main method
- * @return 0
- */
- typedef struct intSet{
+typedef struct intSet{
      int index;
      int array[1];
- }intSet;
+}intSet;
+int cmp_int(const void* _a , const void* _b);
+
+
 int cmp_int(const void* _a , const void* _b)
 {
     int* a = (int*)_a;
     int* b = (int*)_b;
     return *a - *b;
 }
+
 int main()
 {
-    //variable name should be meaningful
-//    char *string = "{1,2,3}";
-//    intSet *intSetpointer;
-//    intSetpointer = (intSet *)malloc(sizeof(intSet));
-//    sscanf(string,"{%d,%d,%d}",&intSetpointer->array[1],&intSetpointer->array[2],&intSetpointer->array[3]);
-//
-//    intSetpointer->array[0] = 3;
-////    intSetpointer->array[1] = 1;
-////    intSetpointer->array[2] = 2;
-////    intSetpointer->array[3] = 3;
-//    for (int i=0; i<4; i++)
-//    {
-//        printf("array[%d]=%d\n",i,intSetpointer->array[i]);
-//    }
-
-//    int array1[]= {3,1,1,1,1};
-//    int array2[] = {2,2,2,2};
-//    int array3[9];
-//    int n1 = 5;
-//    int index1 = 0, index2=0, index3=0;
-//    int n2 = 4;
-//    int n3 = 9;
-//    //sort the array1 and array2
-//    qsort(array1,n1,sizeof(array1[0]),cmp_int);
-//    qsort(array2,n2,sizeof(array2[0]),cmp_int);
-/*
- * calculate union set
- */
-//    while(index1 < n1 && index2<n2)
-//    {
-//        if (array1[index1] <=array2[index2] )
-//        {
-//            array3[index3] = array1[index1];
-//            index1++;
-//            index3++;
-//        }
-//        else{
-//            array3[index3] = array2[index2];
-//            index2++;
-//            index3++;
-//        }
-//    }
-//    while (index1<n1)
-//    {
-//        array3[index3] = array1[index1];
-//        index1++;
-//        index3++;
-//    }
-//    while (index2<n2)
-//    {
-//        array3[index3] = array2[index2];
-//        index2++;
-//        index3++;
-//    }
-//    for(int i=0;i<n3; i++)
-//    {
-//        printf("%d\n",array3[i]);
-//    }
-//    printf("n3 = %d\n",n3);
-//
-///*
-// * distinct set array4
-// * n4: the length of array4
-// */
-//    int index_f1 = 0;
-//    int index_f2 = 0;
-//    int index4 = 0;
-//    int n4 = n3;
-//    int array4[n4];
-//    while (index_f1 < n3)
-//    {
-//        if(index_f1 == n3-1)
-//        {
-//            array4[index4] = array3[index_f1];
-//            break;
-//        }
-//        index_f2 = index_f1+1;
-//        while (index_f2 < n3 )
-//        {
-//            if (array3[index_f1] != array3[index_f2])
-//            {
-//                array4[index4] = array3[index_f1];
-//                index4++;
-//                index_f1++;
-//                break;
-//            }
-//            else{
-//                index_f1++;
-//                index_f2++;
-//                n4--;
-//            }
-//        }
-//
-//
-//    }
-//
-//    for(int i=0;i<n4; i++)
-//    {
-//        printf("%d\n n=%d\n",array4[i],n4);
-//    }
+    char str[1024] = "{1, 2  , 3,4 ,11, 22, 1,1, 100,2,3}";
+    char *delim = "{, }";
+    char *token;
+    int *array, length = 2, countNum = 0, f=0;
+    //initial an array to temporarily store the initial int data
+    array = (int *)malloc(sizeof(int)*length);
+    token = strtok(str, delim);
+    while( token != NULL ) {
+        // if the array is full, realloc the new size
+        if(countNum == length-1)
+        {
+            length = length*2;
+            array = realloc(array,sizeof(int)*length);
+        }
+        // check if the value is distinct
+        for (int i=0; i<countNum; i++) {
+            if (array[i] == atoi(token)) {
+                f = 1;
+                break;
+            }
+        }
+        if (!f) {
+            array[countNum] = atoi(token);
+            countNum++;
+        }
+        token = strtok(NULL, delim);
+        f = 0;
+    }
+    // sort the value
+    qsort(array, countNum,sizeof(int), cmp_int);
 
 
+    for(int i=0; i<countNum; i++)
+    {
+        printf("%d\n", array[i]);
+    }
+    printf("count = %d",countNum);
+
+    free(array);
 
 
-
-
-    return 0;
 }
 
