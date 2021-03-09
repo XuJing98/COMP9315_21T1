a7e0da1a102cacea956a7f7014d8d99c5965eb22
Xu Jing <13309618@student.uts.edu.au>
2021-03-09 21:02:21 +1100

    add && function

--- Codes/A1/test.sql
+++ Codes/A1/test.sql
@@ -5,7 +5,9 @@ insert into mySets values (3, '{5, 2 , 10}');
 insert into mySets values (4, '{5, 2 , 0001, 99999999, 666666, 100000000}');
 insert into mySets values (6, '{5,2}');
 insert into mySets values (5, '{}');
-update mySets set iset = iset || '{6,7,8,9}' where id = 4;
+update mySets set iset = iset || '{7,8,9,2,1}' where id = 4;
+select id, iset, (# iset) as card from mySets order by id;
+update mySets set iset = iset && '{7,8,9,2,1}' where id = 4;
 select id, iset, (# iset) as card from mySets order by id;
 select a.*, b.* from mySets a, mySets b
 where (b.iset @< a.iset) and a.id != b.id;
