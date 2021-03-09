8d07b5840552599ea8f2507ed4ce6e603851d217
Xu Jing <13309618@student.uts.edu.au>
2021-03-09 12:42:23 +1100

    edit shell code

--- Codes/A1/test.sh
+++ Codes/A1/test.sh
@@ -1,4 +1,4 @@
-ssh grieg
+
 cp intset.c /srvr/z5304662/postgresql-12.5/src/tutorial/
 cp intset.source /srvr/z5304662/postgresql-12.5/src/tutorial/
 cp test.sql /srvr/z5304662/postgresql-12.5/src/tutorial/
@@ -11,3 +11,4 @@ dropdb test
 createdb test
 psql test -f intset.sql
 psql test -f test.sql
+pgs stop
