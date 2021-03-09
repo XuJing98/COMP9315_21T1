4bf5adf7c6beab2a89d78b47c58f09e23b9d05b9
Xu Jing <13309618@student.uts.edu.au>
2021-03-09 15:32:31 +1100

    modify sql

--- Codes/A1/test.sql
+++ Codes/A1/test.sql
@@ -2,6 +2,7 @@ create table mySets (id integer primary key, iset intSet);
 insert into mySets values (1,'{1 ,2,3}');
 insert into mySets values (2,'{1,1,1}');
 insert into mySets values (3, '{5, 2 , 10}');
+insert into mySets values (3, '{5, 2 , 0001, 99999999, 666666, 100000000}');
 insert into mySets values (4, '{}');
 select id, iset, (# iset) as card from mySets order by id;
 
